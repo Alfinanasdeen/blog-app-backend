3d7332d2763293c1b86980fee5365ededf1d36bd
Alfinanasdeen <alfinanasdeen@gmail.com>
2024-10-15 14:35:20 +0530

    commit

--- data.config.js
+++ data.config.js
@@ -23,7 +23,7 @@ function connectToMongoDB() {
   const DATABASE_URI =
     process.env.NODE_ENV === "development"
       ? process.env.MONGODB_URI
-      : `mongodb+srv://${process.env.MONGODB_NAME}:${process.env.MONGODB_PASSWORD}@cluster0.3kvjo.mongodb.net/`;
+      : `mongodb+srv://${process.env.MONGODB_NAME}:${process.env.MONGODB_PASSWORD}@cluster0.3kvjo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
   mongoose
     .connect(DATABASE_URI)
